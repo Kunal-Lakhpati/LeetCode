61d2e96befc6f693cac472ba67debb1070b785ef
Kunal Lakhpati <lakhskunal1094@gmail.com>
2025-01-03 22:44:53 +0530

    Add files via upload

---
+++ 53.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum=nums[0];
+        int max=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            if(sum+nums[i]<nums[i])
+            {
+                sum=nums[i];
+            }
+            else
+            {
+                sum+=nums[i];
+            }
+            if(max<sum)
+            {
+                max=sum;
+            }
+        }
+        return max;
+    }
+};
