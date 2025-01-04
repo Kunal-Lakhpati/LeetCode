d8e35cb3dbf1239e2e3865cbec6ef112e2e92553
Kunal Lakhpati <lakhskunal1094@gmail.com>
2025-01-04 16:39:22 +0530

    Add files via upload

---
+++ 1217.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& position) {
+        int even=0;
+        int odd=0;
+        int cost=0;
+        for(int i=0;i<position.size();i++)
+        {
+            if(position[i]%2==0)
+            {
+                even++;
+            }
+            else
+            {
+                odd++;
+            }
+            if(even>odd)
+            {
+                cost=odd;
+            }
+            else if(even==odd)
+            {
+                cost=even;
+            }
+            else
+            {
+                cost=even;
+            }
+        }
+        return cost;
+    }
+};
