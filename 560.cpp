bc396c784ec7b3d0875b2b9fe88e6189453a6a78
Kunal Lakhpati <lakhskunal1094@gmail.com>
2025-01-13 23:15:31 +0530

    Add files via upload

---
+++ 560.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int sum=0;
+        int count=0;
+        for(int i=0;i<nums.size();i++)//i=0
+        {
+            for(int j=i;j<nums.size();j++)//j=1
+            {
+                sum=sum+nums[j];
+                if(sum==k)
+                {
+                    count++;
+                }
+            }
+            sum=0;
+        }
+        return count;
+    }
+};
