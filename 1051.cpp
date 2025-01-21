a532edc54eb1ed03da1ece54f8deb76c9c9365cb
Kunal Lakhpati <lakhskunal1094@gmail.com>
2025-01-21 21:39:31 +0530

    Add files via upload

---
+++ 1051.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int count=0;
+        int n=heights.size();
+        vector<int> news(n);
+        for(int i=0;i<heights.size();i++)
+        {
+            news[i]=heights[i];
+        }
+        sort(heights.begin(),heights.end());
+        for(int i=0;i<heights.size();i++)
+        {
+            if(heights[i]!=news[i])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
