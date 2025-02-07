1f5c1b399d04042341c2504d944356b8cb0d01a3
Kunal Lakhpati <lakhskunal1094@gmail.com>
2025-02-07 23:01:09 +0530

    Add files via upload

--- 215.cpp
+++ 215.cpp
@@ -1,34 +1,18 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end(), greater<int>());
-        return nums[k-1];
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = i + 1; j <= i + k && j < n; j++) 
+            {
+                if (nums[i] == nums[j]) 
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 };
-        // int max=INT_MIN;
-        // int count=0;
-        // for(int j=0;j<nums.size();j++)
-        // {
-        //     if(max<nums[j])
-        //     {
-        //         max=nums[j];
-        //     }
-        // }
-        // while(true)
-        // {
-        //     count=0;
-        // for (int i=0;i<nums.size();i++)
-        // {
-        //     if(nums[i]==max)
-        //     {
-        //         count++;
-        //     }
-        // }
-        //     if(count>=k)
-        //     {
-        //         return max;
-        //     }
-        //     k=k-count;
-        //     max--;
-        // }
-        // return max;
