2e370d2df1d2e84e066df23dbf725453f6368137
Kunal Lakhpati <lakhskunal1094@gmail.com>
2025-03-22 00:53:01 +0530

    Update 122.cpp

--- 122.cpp
+++ 122.cpp
@@ -14,6 +14,7 @@ public:
         for(int i=0;i<prices.size()-1;i++)
         {
             if(prices[i]<prices[i+1])
+                
             {
                 profit+=(prices[i+1]-prices[i]);
             } 
