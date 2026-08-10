6be79eeb62091b8c80a4c507a2f5491b1bd5688b
Kunal Lakhpati <lakhskunal1094@gmail.com>
2026-08-10 19:31:14 +0530

    Time: 3 ms (98.53%), Space: 9 MB (73.64%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+class Solution {
+public:
+    bool winnerSquareGame(int n) 
+    {
+        std::vector<bool> dp(n + 1, false);
+        for (int i = 0; i <= n; i++) 
+        {
+            if (!dp[i]) 
+            {
+                for (int j = 1; i + j * j <= n; j++) 
+                {
+                    dp[i + j * j] = true;
+                }
+                if (dp[n]) 
+                {
+                    return true;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
